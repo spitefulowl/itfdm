022814aac0cfab223ae7e7c15b741ea15d4f0023
spitefulowll <spitefulowll@gmail.com>
2021-10-25 00:08:38 +0300

    Optimizations

--- DeliveryProblem/branch_and_bound.py
+++ DeliveryProblem/branch_and_bound.py
@@ -1,22 +1,37 @@
 from utils import get_descendants
 
+min_upper_bound = 9999999
+min_upper_bound_updated = False
+min_upper_bound_item = None
+
 class BranchAndBound():
     @staticmethod
-    def _check_vertices(current_vertices, lower_bound, upper_bound):
-        min_upper_bound = upper_bound.task.size
-        for item in current_vertices:
-            current_upper_bound = upper_bound.get(item)[0]
+    def _check_vertices(current_vertices, current_vertex, descendants, lower_bound, upper_bound):
+        global min_upper_bound
+        global min_upper_bound_updated
+        global min_upper_bound_item
+        min_upper_bound_updated = False
+        for descendant in descendants:
+            current_new_vertex = current_vertex + (descendant,)
+            current_upper_bound = upper_bound.get(current_new_vertex)[0]
             if current_upper_bound <= min_upper_bound:
                 min_upper_bound = current_upper_bound
-                min_upper_bound_item = item
+                min_upper_bound_item = current_new_vertex
+                min_upper_bound_updated = True
 
-        to_drop = []
-        for vertex in current_vertices:
-            if lower_bound.get(vertex) >= min_upper_bound and min_upper_bound_item != vertex:
-                to_drop.append(vertex)
+        if min_upper_bound_updated:
+            to_drop = []
+            for vertex in current_vertices:
+                if lower_bound.get(vertex) >= min_upper_bound and min_upper_bound_item != vertex:
+                    to_drop.append(vertex)
 
-        for item in to_drop:
-            current_vertices.remove(item)
+            for item in to_drop:
+                current_vertices.remove(item)
+
+        for descendant in descendants:
+            current_new_vertex = current_vertex + (descendant,)
+            if lower_bound.get(current_new_vertex) < min_upper_bound or min_upper_bound_item == current_new_vertex:
+                current_vertices.add(current_new_vertex)
 
     @staticmethod
     def solve(size, strategy, lower_bound, upper_bound):
@@ -36,8 +51,7 @@ class BranchAndBound():
             current_vertex = strategy.get(current_vertices)
             current_vertices.discard(current_vertex)
             descendants = get_descendants(size, current_vertex)
-            current_vertices.update([current_vertex + (descendant,) for descendant in descendants])
-            BranchAndBound._check_vertices(current_vertices, lower_bound, upper_bound)
+            BranchAndBound._check_vertices(current_vertices, current_vertex, descendants, lower_bound, upper_bound)
             iterations += 1
 
         return solution, iterations
--- DeliveryProblem/tree_traversals.py
+++ DeliveryProblem/tree_traversals.py
@@ -5,8 +5,8 @@ class BreadthFirst():
     def get(self, current_vertices):
         if not current_vertices:
             return tuple()
-        if len(current_vertices) == 1:
-            print('FATAL: not solution')
-            exit(-1)
+        # if len(current_vertices) == 1:
+        #     print('FATAL: not solution')
+        #     exit(-1)
 
         return min(current_vertices, key=lambda x: len(x))
