83ca35571a58a072b22c0df6b0c06037d3ab9d55
spitefulowll <spitefulowll@gmail.com>
2021-10-31 17:27:58 +0300

    Fix runner

--- DeliveryProblemCXX/runner.py
+++ DeliveryProblemCXX/runner.py
@@ -10,7 +10,7 @@ CRIT_PATTERN = re_compile("Crit: (\\d+)")
 TASK_SIZE_PATTERN = re_compile("task_2_\\d+_n(\\d+).txt")
 
 ALGO_SET = ("base","custom")
-CSV_HEADER = ["n", "base_iter", "ratio", "custom_iter", "ratio"]
+CSV_HEADER = ["n", "base_iter", "custom_iter", "ratio"]
 FLOAT_LENGTH = 9
 
 def parse_arguments():
@@ -31,7 +31,6 @@ def save_csv(results):
         _file.write(','.join(CSV_HEADER) + '\n')
         base_results = results[ALGO_SET[0]]
         custom_results = results[ALGO_SET[1]]
-        base_ratio_sum = 0
         custom_ratio_sum = 0
         for task in results[ALGO_SET[0]].keys():
             size = int(base_results[task][2])
@@ -39,18 +38,14 @@ def save_csv(results):
             custom_iterations = int(custom_results[task][0])
             number_of_solutions = math_factorial(size)
 
-            base_ratio = base_iterations / number_of_solutions - 1
-            custom_ratio = custom_iterations / number_of_solutions - 1
-
-            base_ratio_sum += base_ratio
+            custom_ratio = custom_iterations / base_iterations
             custom_ratio_sum += custom_ratio
 
-            _file.write(','.join(map(str, [size, base_iterations, str(base_ratio)[:FLOAT_LENGTH],
+            _file.write(','.join(map(str, [size, base_iterations,
                 custom_iterations, str(custom_ratio)[:FLOAT_LENGTH], '\n'])))
 
-        base_ratio_avg = base_ratio_sum / len(results[ALGO_SET[0]])
         custom_ratio_avg = custom_ratio_sum / len(results[ALGO_SET[0]])
-        _file.write(','.join(map(str, ['', '', base_ratio_avg, '', custom_ratio_avg])))
+        _file.write(','.join(map(str, ['', '', '', custom_ratio_avg])))
 
 def main():
     args = parse_arguments()
